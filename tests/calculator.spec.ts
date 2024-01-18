19c0ec755c4fa40718a99fad5b997a09b6f64ee5
Ashis Raj <ashis.raj@nuorder.com>
2024-01-18 14:30:54 +0530

    Initial commit

---
+++ tests/calculator.spec.ts
@@ -0,0 +1,17 @@
+import { divide, multiply, subtract, sum } from "../src/calculator";
+var assert = require('assert');
+
+describe('Index tests', function() {
+    it('2 + 2 should return 4', function() {
+        assert.equal(sum(2, 2), 4);
+    });
+    it('2 - 2 should return 0', function() {
+        assert.equal(subtract(2, 2), 0);
+    });
+    it('2 * 2 should return 4', function() {
+        assert.equal(multiply(2, 2), 4);
+    });
+    it('2 / 2 should return 1', function() {
+        assert.equal(divide(2, 2), 1);
+    });
+})
